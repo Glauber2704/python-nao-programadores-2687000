028c00d27bc25734203f8cce68ff5ca3a35d8c6b
Glauber2704 <glauberssoares27@gmail.com>
2025-07-22 02:00:13 +0000

    Resolução

--- gabaritos/1-primeiros-passos/4-operadores_resposta.py
+++ gabaritos/1-primeiros-passos/4-operadores_resposta.py
@@ -3,7 +3,7 @@ ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
 idade = ano_formatura - ano_nascimento
-
+print(ano_formatura - ano_nascimento)
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
 print(ano_nascimento > ano_formatura)
 print(ano_nascimento <= ano_formatura)
