8b9cde3d8b295d88a51361e5360b629bc33fcab5
Glauber2704 <glauberssoares27@gmail.com>
2025-07-23 02:16:10 +0000

    Resolução

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -2,10 +2,16 @@
 
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
 Estudante ={ }
-ome = input("Qual o seu nome?")ome = input("Qual o seu nome?")
-ano_linkedIn = input("Qual ano você conheceu o LinkedIn?")
-ano_atual = input("Qual o ano atual?")
+Estudante['nome'] = input("Qual o seu nome?")
+Estudante['ano_linkedIn'] = int(input("Qual ano você conheceu o LinkedIn?"))
+Estudante['ano_atual'] = int(input("Qual o ano atual?"))
 cursos = input("Quais cursos você já fez?")
+
+Estudante['cursos'] = cursos.split(', ')
 # 2. Armazene esses dados em um dicionário
 
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
+Total_anos = Estudante['ano_atual'] - Estudante['ano_linkedIn']
+total_cursos = len(Estudante['cursos'])
+
+print(f"Oi {Estudante['nome']}, desde {Estudante['ano_linkedIn']} você conhece o LinkedIn. Nesses {Total_anos} anos, você realizou {total_cursos} cursos, sendo o primeiro curso {Estudante['cursos'][0]} e o último curso {Estudante['cursos'][-1]}")
