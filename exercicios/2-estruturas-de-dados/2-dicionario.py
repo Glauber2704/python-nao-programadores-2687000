5292d96915ce5fca14ef1e99597f6a0192cc10be
Glauber2704 <glauberssoares27@gmail.com>
2025-07-23 01:48:15 +0000

    Resolução

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -7,12 +7,17 @@ pessoa = {'nome':'Crislaine',
           'animal_estimacao':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print(pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
+print(list(pessoa.values()))
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
-
+print(pessoa.keys())
+print(list(pessoa.keys()))
 # Insira um novo par chave-valor no dicionário
+pessoa['ano_formatura'] = 2002
+print(pessoa["ano_formatura"])
+
+pessoa.pop('idade')
+print(list(pessoa.keys()))
