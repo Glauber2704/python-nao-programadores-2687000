3d7f4389f54303f463f6e8dea02ff537ddd9d695
Glauber2704 <glauberssoares27@gmail.com>
2025-07-24 00:45:49 +0000

    Resolução

--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -1,5 +1,21 @@
 # Criaremos um programa para substituir números por palavras em uma lista
 # 1. Crie uma lista com 15 números
+lista_números = list(range(15,31))
+indice = 0
+print(lista_números)
+
+for número in lista_números:
+  if número % 3 == 0 and número % 5 ==0:
+    lista_números[indice] = 'FizzBuzz'
+  elif número % 3 == 0:
+    lista_números[indice] = 'Fizz'
+  elif número % 5 == 0:
+    lista_números[indice] = 'Buzz'
+  else:
+    lista_números[indice] = número
+  indice += 1
+print(lista_números)
+
 # 2. Crie um for loop para percorrer todos os elementos da lista
 # 3. Crie uma estrutura condicional para verificar cada número da lista:
 # 3.1 Caso o número seja divisível por 3, substitua-o por "Fizz"
