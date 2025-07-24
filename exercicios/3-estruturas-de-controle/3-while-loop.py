26e1297824101444cc0948b933733a05d6f14fd9
Glauber2704 <glauberssoares27@gmail.com>
2025-07-24 00:32:30 +0000

    resolução

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,13 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
-
+cursos_linkedin = ['Riscos', 'Controles', 'Auditoria', 'GRC', 'Seguros']
+for curso in cursos_linkedin:
+  print(curso)
 # Crie um while loop que imprima na tela o nível atual
+print(list(range(5)))
+print(list(range(12,20,2)))
 
-
+for item in range(3,10,3):
+  print(f'{item} número')
+else:
+  print('Fim do loop')
 # Insira "else" no while loop anterior.
